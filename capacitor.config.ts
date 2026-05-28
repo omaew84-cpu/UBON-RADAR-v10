63fb05e64b8d4fa0c1ae8279ce19226708a045cc
omaew84-cpu <omaew84@gmail.com>
2026-05-28 09:38:24 +0700

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ubon.radar.v10',
+  appName: 'UBON RADAR v10',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
